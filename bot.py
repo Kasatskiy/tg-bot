20b5293a2eeaff85114a23c2864e18163ed425ff
MacBook <macbook@MacBook-MacBook-Pro.local>
2026-03-20 13:48:52 +0100

    final bot code

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from aiogram.types import (
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8710757819:AAFra83pBHkxPT9m6BYJRY9kEh8Akry39gI")
 DB_NAME = os.getenv("DB_NAME", "bot.db")
 
 MAX_REMINDERS = 10
