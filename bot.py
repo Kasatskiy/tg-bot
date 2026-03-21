11a1ccce0b152a268533bf7c8d2c6cc9410f03c4
MacBook <macbook@MacBook-MacBook-Pro.local>
2026-03-21 20:08:19 +0100

    final bot code

--- bot.py
+++ bot.py
@@ -20,9 +20,7 @@ from aiogram.types import (
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN is not set")
+BOT_TOKEN = "8710757819:AAFra83pBHkxPT9m6BYJRY9kEh8Akry39gI"
 
 DB_NAME = os.getenv("DB_NAME", "bot.db")
 
